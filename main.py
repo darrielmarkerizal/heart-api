65a70f2f8955c74f325fe3435b3b23491c9e14cb
Darriel Markerizal <darrielmarkerizal@gmail.com>
2025-06-29 10:49:51 +0800

    render

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from pydantic import BaseModel
 import numpy as np
 import joblib
 
-app = FastAPI()
-
-# Load model once at startup
+# Load model
 model = joblib.load("heart_model_xgb.pkl")
 
+app = FastAPI(title="Heart Attack Prediction API")
+
+# Define input format
 class HeartData(BaseModel):
     age: int
     gender: int  # 1 = Male, 0 = Female
@@ -19,20 +20,20 @@ class HeartData(BaseModel):
     troponin: float
 
 @app.get("/")
-def home():
-    return {"message": "API for Heart Attack Prediction is active"}
+def read_root():
+    return {"message": "Welcome to Heart Attack Prediction API!"}
 
 @app.post("/predict")
 def predict(data: HeartData):
     flag_ckmb = 1 if data.ckmb > 50 else 0
     flag_trop = 1 if data.troponin > 0.4 else 0
 
-    input_array = np.array([[data.age, data.gender, data.heart_rate,
-                             data.systolic_bp, data.diastolic_bp,
-                             data.blood_sugar, data.ckmb, data.troponin,
+    input_array = np.array([[data.age, data.gender, data.heart_rate, data.systolic_bp,
+                             data.diastolic_bp, data.blood_sugar, data.ckmb, data.troponin,
                              flag_ckmb, flag_trop]])
-    result = model.predict(input_array)[0]
+    pred = model.predict(input_array)[0]
+
     return {
-        "prediction": int(result),
-        "risk": "High" if result == 1 else "Low"
+        "prediction": int(pred),
+        "risk": "High" if pred == 1 else "Low"
     }
