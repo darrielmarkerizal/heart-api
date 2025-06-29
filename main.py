9ca41ad68f35baa0788ca66b380013394a9caf43
Darriel Markerizal <darrielmarkerizal@gmail.com>
2025-06-29 10:40:23 +0800

    railway

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ model = joblib.load("heart_model_xgb.pkl")
 
 app = FastAPI(title="Heart Attack Prediction API")
 
-# Define input format
 class HeartData(BaseModel):
     age: int
-    gender: int  # 1 = Male, 0 = Female
+    gender: int      # 1 = Male, 0 = Female
     heart_rate: int
     systolic_bp: int
     diastolic_bp: int
@@ -20,20 +19,21 @@ class HeartData(BaseModel):
     troponin: float
 
 @app.get("/")
-def read_root():
-    return {"message": "Welcome to Heart Attack Prediction API!"}
+def root():
+    return {"message": "Heart Attack Prediction API is running."}
 
 @app.post("/predict")
 def predict(data: HeartData):
     flag_ckmb = 1 if data.ckmb > 50 else 0
     flag_trop = 1 if data.troponin > 0.4 else 0
 
-    input_array = np.array([[data.age, data.gender, data.heart_rate, data.systolic_bp,
-                             data.diastolic_bp, data.blood_sugar, data.ckmb, data.troponin,
+    input_array = np.array([[data.age, data.gender, data.heart_rate,
+                             data.systolic_bp, data.diastolic_bp,
+                             data.blood_sugar, data.ckmb, data.troponin,
                              flag_ckmb, flag_trop]])
-    pred = model.predict(input_array)[0]
+    prediction = model.predict(input_array)[0]
 
     return {
-        "prediction": int(pred),
-        "risk": "High" if pred == 1 else "Low"
+        "prediction": int(prediction),
+        "risk": "High" if prediction == 1 else "Low"
     }
